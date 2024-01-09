e308da533d90af53729d45adfdee6c46bb88a8a7
KangMinSung <43738417+KangMinSung720@users.noreply.github.com>
2024-01-09 19:22:25 +0900

    Initial Commit

---
+++ chukcheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chukcheck
+//
+//  Created by 강민성 on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chukcheck/chukcheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  chukcheckApp.swift
+//  chukcheck
+//
+//  Created by 강민성 on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct chukcheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
